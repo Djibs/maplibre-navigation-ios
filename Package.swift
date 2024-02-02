511a55a79d357383ba4615d0dff352ad666d70b7
djibs <iphoneaddict.fr@gmail.com>
2024-02-02 18:11:36 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,12 +10,11 @@ let package = Package(
         ),
     ],
     dependencies: [
-        // Dépendances à d'autres packages.
-        .package(url: "https://github.com/mapbox/MapboxGeocoder.swift", from: "0.15.0"),
-        .package(url: "https://github.com/flitsmeister/Mapbox-directions-swift", .exact("0.23.2")),
-        .package(url: "https://github.com/flitsmeister/turf-swift", .exact("0.2.2")),
-        .package(url: "https://github.com/ceeK/Solar", from: "3.0.1")
-    ],
+            .package(url: "https://github.com/mapbox/MapboxGeocoder.swift", from: "0.15.0"),
+            .package(url: "https://github.com/flitsmeister/Mapbox-directions-swift", .upToNextMajor(from: "0.23.2")),
+            .package(url: "https://github.com/flitsmeister/turf-swift", .upToNextMajor(from: "0.2.2")),
+            .package(url: "https://github.com/ceeK/Solar", from: "3.0.1")
+        ],
     targets: [
         .target(
             name: "MapboxNavigation",
